8fdc19530353cbb6580866cc0a4b50f94090fc6d
Paul Gibbs <paul.gibbs@indigotree.co.uk>
2022-02-17 18:49:39 +0000

    Stylelint: ignore another rule.

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -4,6 +4,7 @@ module.exports = {
     "scss/comment-no-empty": null,
     "scss/at-import-partial-extension": null,
     "scss/no-global-function-names": null,
-    "scss/at-extend-no-missing-placeholder": null
+    "scss/at-extend-no-missing-placeholder": null,
+    "scss/operator-no-unspaced": null
   }
 };
