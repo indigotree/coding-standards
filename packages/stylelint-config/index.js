34d69a2b7a52eb1273b38d9cc7523f4291585681
Paul Gibbs <paul.gibbs@indigotree.co.uk>
2022-02-17 18:35:09 +0000

    Stylelint: update rules.

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ["stylelint-config-recommended-scss", "stylelint-config-prettier"],
+  rules: {
+    "scss/comment-no-empty": null,
+    "scss/at-import-partial-extension": null,
+    "scss/no-global-function-names": null,
+    "scss/at-extend-no-missing-placeholder": null
+  }
 };
